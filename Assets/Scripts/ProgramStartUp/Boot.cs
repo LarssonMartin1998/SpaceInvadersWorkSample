f1642ac1ef868d5d273679b0178e81e8088e8d78
Martin Larsson <larssonmartin1998@gmail.com>
2018-09-28 12:51:18 +0200

    add boot scene and it's scripts.

---
+++ Assets/Scripts/ProgramStartUp/Boot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boot : MonoBehaviour
+{
+    [SerializeField]
+    private SceneLoader sceneLoader;
+
+    private bool shuttingDown = false;
+
+    public void Shutdown()
+    {
+        sceneLoader.Shutdown();
+        shuttingDown = true;
+    }
+
+    private void Start ()
+    {
+        StartCoroutine(sceneLoader.Initialize());
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Shutdown();
+        }
+
+        if (!shuttingDown)
+        {
+            return;
+        }
+
+        // If scenecount is equal to 1 it means that all other scenes succesfully shut down.
+        if (SceneManager.sceneCount == 1)
+        {
+            // Perfect place to check for leaks etc.
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/ProgramStartUp/SceneLoader.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    private int staticBuildIndex;
+    [SerializeField]
+    private int gameBuildIndex;
+
+    private Dictionary<int, Scene> scenes;
+    bool sceneLoaded = false;
+
+    public IEnumerator Initialize()
+    {
+        scenes = new Dictionary<int, Scene>();
+
+        StartCoroutine(LoadSceneInternal(staticBuildIndex));
+
+        // Wait for scene to finish loading ...
+        while(!sceneLoaded)
+        {
+            yield return null;
+        }
+
+        StartCoroutine(LoadSceneInternal(gameBuildIndex));
+
+        // Wait for scene to finish loading ...
+        while (!sceneLoaded)
+        {
+            yield return null;
+        }
+
+        SceneManager.SetActiveScene(scenes[gameBuildIndex]);
+        EventManager.instance.TriggerEvent(AllEventTypes.EVENT_GAME_SCENE_LOADED);
+    }
+
+    // Will set the current null scene variable to its proper value
+    private IEnumerator LoadSceneInternal(int buildIndex)
+    {
+        sceneLoaded = false;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(buildIndex, LoadSceneMode.Additive);
+        operation.allowSceneActivation = true;
+
+        while (operation.progress < 1f)
+        {
+            yield return null;
+        }
+
+        Scene scene = SceneManager.GetSceneByBuildIndex(buildIndex);
+        scenes.Add(buildIndex, scene);
+
+        sceneLoaded = true;
+    }
+
+    public void Shutdown()
+    {
+        foreach (Scene scene in scenes.Values)
+        {
+            StartCoroutine(ShutdownSceneInternal(scene));
+        }
+
+        scenes.Clear();
+    }
+
+    private IEnumerator ShutdownSceneInternal(Scene scene)
+    {
+        AsyncOperation operation = SceneManager.UnloadSceneAsync(scene);
+
+        while (operation.progress < 0.9f)
+        {
+            yield return null;
+        }
+    }
+}
