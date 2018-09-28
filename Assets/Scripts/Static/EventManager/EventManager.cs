ce3aaf583097d6033cc288fc807847099b1bd8eb
Martin Larsson <larssonmartin1998@gmail.com>
2018-09-28 12:51:58 +0200

    add static scene and event manager

---
+++ Assets/Scripts/Static/EventManager/EventManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager instance;
+
+    private Dictionary<AllEventTypes, UnityEvent> eventDictionary;
+
+    private void Awake()
+    {
+        InitializeThisSingleton();
+
+        eventDictionary = new Dictionary<AllEventTypes, UnityEvent>();
+    }
+
+    private void InitializeThisSingleton()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        // Means that there already is an instance of this singleton and were trying to create another.
+        else if (instance != null)
+        {
+            Debug.Log("Tried to initialize an already created singleton: \"EventManager\"");
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartListening(AllEventTypes eventType, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (eventDictionary.TryGetValue(eventType, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            eventDictionary.Add(eventType, thisEvent);
+        }
+    }
+
+    public void StopListening(AllEventTypes eventType, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (eventDictionary.TryGetValue(eventType, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public void TriggerEvent(AllEventTypes eventType)
+    {
+        UnityEvent thisEvent = null;
+        if (eventDictionary.TryGetValue(eventType, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+    }
+}
